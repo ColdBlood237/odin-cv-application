9de41531991f83ffa4d1f7ed1765b678fe4b6659
Pixel <petngalan@gmail.com>
2023-05-29 09:54:32 +0200

    Rewrite the generalInfo component in functional component

--- src/components/GeneraInfo.js
+++ src/components/GeneraInfo.js
@@ -1,62 +1,60 @@
 import React, { Component } from "react";
 
-export default class GeneralInfo extends Component {
-  render() {
-    const { handleChange } = this.props;
+export default function GeneralInfo(props) {
+  const { handleChange } = props;
 
-    return (
-      <div>
-        <h3>Personal Information</h3>
-        <form>
-          <input
-            onChange={handleChange}
-            type="text"
-            placeholder="First name"
-            id="fname"
-          ></input>
-          <input
-            onChange={handleChange}
-            type="text"
-            placeholder="Last name"
-            id="lname"
-          ></input>
-          <input
-            onChange={handleChange}
-            type="text"
-            placeholder="Title"
-            id="title"
-          ></input>
-          <input
-            onChange={handleChange}
-            type="text"
-            placeholder="Profile picture url"
-            id="img-url"
-          ></input>
-          <input
-            onChange={handleChange}
-            type="text"
-            placeholder="Address"
-            id="address"
-          ></input>
-          <input
-            onChange={handleChange}
-            type="number"
-            placeholder="Phone number"
-            id="phone"
-          ></input>
-          <input
-            onChange={handleChange}
-            type="email"
-            placeholder="Email"
-            id="email"
-          ></input>
-          <textarea
-            onChange={handleChange}
-            placeholder="Description"
-            id="description"
-          ></textarea>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h3>Personal Information</h3>
+      <form>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="First name"
+          id="fname"
+        ></input>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Last name"
+          id="lname"
+        ></input>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Title"
+          id="title"
+        ></input>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Profile picture url"
+          id="img-url"
+        ></input>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Address"
+          id="address"
+        ></input>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Phone number"
+          id="phone"
+        ></input>
+        <input
+          onChange={handleChange}
+          type="email"
+          placeholder="Email"
+          id="email"
+        ></input>
+        <textarea
+          onChange={handleChange}
+          placeholder="Description"
+          id="description"
+        ></textarea>
+      </form>
+    </div>
+  );
 }
